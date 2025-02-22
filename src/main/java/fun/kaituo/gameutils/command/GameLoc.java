d0201adaa40a2abd5aa09f580d025db44866766a
YFshadaow <renzd2001@gmail.com>
2025-02-22 21:02:31 +0800

    /gameloc commands now have block/player mode. Also tab complete when using /gameitem save and /gameloc save, to allow more convenient overwriting.

--- src/main/java/fun/kaituo/gameutils/command/GameLoc.java
+++ src/main/java/fun/kaituo/gameutils/command/GameLoc.java
@@ -3,6 +3,7 @@ package fun.kaituo.gameutils.command;
 import fun.kaituo.gameutils.GameUtils;
 import fun.kaituo.gameutils.game.Game;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,6 +19,7 @@ public class GameLoc implements CommandExecutor, TabCompleter {
 
     public static final String PERMISSION = "gameutils.command.gameloc";
     private final List<String> actions = Arrays.asList("save", "tp", "remove");
+    private final List<String> saveModes = Arrays.asList("block", "player");
 
 
     @Override
@@ -34,19 +36,22 @@ public class GameLoc implements CommandExecutor, TabCompleter {
             sender.sendMessage("§c你没有权限执行这个指令！");
             return true;
         }
-        if (args.length != 2) {
-            sender.sendMessage("§c指令参数错误！使用方法为/gameloc <save/get/remove> <位置ID>");
+        if (args.length != 2 && args.length != 3) {
+            sender.sendMessage("§c指令参数错误！使用方法为/gameloc <save/get/remove> <位置ID> [block/player]");
             return true;
         }
         if (!args[0].equalsIgnoreCase("save") &&
             !args[0].equalsIgnoreCase("tp") &&
             !args[0].equalsIgnoreCase("remove")) {
-            sender.sendMessage("§c指令参数错误！使用方法为/gameloc <save/get/remove> <位置ID>");
+            sender.sendMessage("§c指令参数错误！使用方法为/gameloc <save/get/remove> <位置ID> [block/player]");
             return true;
-
         }
         Game game = GameUtils.inst().getGame(p);
         if (args[0].equalsIgnoreCase("tp")) {
+            if (args.length == 3) {
+                sender.sendMessage("§c指令参数错误！使用方法为/gameloc tp <位置ID>");
+                return true;
+            }
             Location loc = game.getLoc(args[1]);
             if (loc == null) {
                 sender.sendMessage("§c未找到ID为 " + args[1] + " 的位置！");
@@ -55,9 +60,31 @@ public class GameLoc implements CommandExecutor, TabCompleter {
             p.teleport(loc);
             p.sendMessage("§a成功传送到ID为 " + args[1] + " 的位置！");
         } else if (args[0].equalsIgnoreCase("save")) {
-            game.saveLoc(args[1], p.getLocation());
+            boolean blockMode = false;
+            if (args.length == 3) {
+                if (args[2].equalsIgnoreCase("block")) {
+                    blockMode = true;
+                }
+            }
+            Location loc;
+            if (blockMode) {
+                Block b = p.getTargetBlockExact(5);
+                if (b == null) {
+                    p.sendMessage("§c未找到目标方块！");
+                    return true;
+                }
+                loc = b.getLocation();
+                p.sendMessage("§f目标方块为 " + b.getType().name());
+            } else {
+                loc = p.getLocation();
+            }
+            game.saveLoc(args[1], loc);
             p.sendMessage("§a成功将当前位置保存到 " + game.getName() + " 的位置列表，ID为 " + args[1]);
         } else if (args[0].equalsIgnoreCase("remove")) {
+            if (args.length == 3) {
+                sender.sendMessage("§c指令参数错误！使用方法为/gameloc remove <位置ID>");
+                return true;
+            }
             if (game.removeLoc(args[1])) {
                 p.sendMessage("§a成功移除ID为 " + args[1] + " 的位置！");
             } else {
@@ -74,9 +101,11 @@ public class GameLoc implements CommandExecutor, TabCompleter {
         if (args.length == 1) {
             return actions;
         } else if (args.length == 2) {
-            if (args[0].equalsIgnoreCase("tp") || args[0].equalsIgnoreCase("remove")) {
-                Game game = GameUtils.inst().getGame((Player) sender);
-                return new ArrayList<>(game.getLocIds());
+            Game game = GameUtils.inst().getGame((Player) sender);
+            return new ArrayList<>(game.getLocIds());
+        } else if (args.length == 3) {
+            if (args[0].equalsIgnoreCase("save")) {
+                return saveModes;
             }
         }
         return new ArrayList<>();
