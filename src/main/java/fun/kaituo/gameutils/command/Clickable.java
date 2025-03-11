8a87d4dc3d6e5ad88d5f3bdac6fd1daf00537092
zhengdao-ren <zhengdao.ren@thermofisher.com>
2025-03-11 14:49:32 +0800

    Add /droppable and /clickable to limit item dropping and clicking.

---
+++ src/main/java/fun/kaituo/gameutils/command/Clickable.java
@@ -0,0 +1,80 @@
+package fun.kaituo.gameutils.command;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static fun.kaituo.gameutils.util.Misc.*;
+
+@SuppressWarnings("unused")
+public class Clickable extends GameUtilsCommand implements TabCompleter {
+    @Override
+    public String getName() {
+        return "clickable";
+    }
+
+    private final List<String> booleans = Arrays.asList("true", "false");
+
+    @Override
+    public boolean onCommand(@Nonnull CommandSender sender, Command cmd, @Nonnull String label, @Nonnull String[] args) {
+        if (!cmd.getName().equalsIgnoreCase(getName())) {
+            return false;
+        }
+
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage("§c此指令必须由玩家执行！");
+            return true;
+        }
+        if (!sender.hasPermission(getPermissionString())) {
+            sender.sendMessage("§c你没有权限执行这个指令！");
+            return true;
+        }
+        if (args.length > 1) {
+            sender.sendMessage("§c指令参数错误！使用方法为/clickable [true/false]");
+            return true;
+        }
+        if (args.length == 1 && !booleans.contains(args[0].toLowerCase())) {
+            sender.sendMessage("§c指令参数错误！使用方法为/clickable [true/false]");
+            return true;
+        }
+        ItemStack item = p.getInventory().getItemInMainHand().clone();
+        if (item.getType().equals(Material.AIR)) {
+            p.sendMessage("§c请使用主手手持物品！");
+            return true;
+        }
+        boolean clickable;
+        if (args.length == 0) {
+            clickable = !isClickable(item);
+        } else {
+            clickable = Boolean.parseBoolean(args[0]);
+        }
+        setClickable(item, clickable);
+        p.getInventory().setItemInMainHand(item);
+
+        if (clickable) {
+            p.sendMessage("§f已设置物品为§a可点击");
+        } else {
+            p.sendMessage("§f已设置物品为§c不可点击");
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(@Nonnull CommandSender commandSender, Command command, @Nonnull String alias, @Nonnull String[] args) {
+        if (!command.getName().equalsIgnoreCase(getName())) {
+            return new ArrayList<>();
+        }
+        if (args.length != 1) {
+            return new ArrayList<>();
+        }
+        return getMatchingCompletions(args[0], booleans);
+    }
+}
