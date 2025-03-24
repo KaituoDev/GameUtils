57a7e46a8e2f04306fb8d56051dc1f8460fd64ec
YFshadaow <renzd2001@gmail.com>
2025-03-24 22:49:09 +0800

    /forcestop can be used without parameter to stop the current game. /join can be used to join another player to a specific game.

--- src/main/java/fun/kaituo/gameutils/command/Join.java
+++ src/main/java/fun/kaituo/gameutils/command/Join.java
@@ -2,6 +2,7 @@ package fun.kaituo.gameutils.command;
 
 import fun.kaituo.gameutils.GameUtils;
 import fun.kaituo.gameutils.game.Game;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -20,23 +21,40 @@ public class Join extends GameUtilsCommand implements TabCompleter {
         return "join";
     }
 
+    public String getOthersPermissionString() {
+        return getPermissionString() + ".others";
+    }
+
     @Override
     public boolean onCommand(@Nonnull CommandSender sender, Command cmd, @Nonnull String label, @Nonnull String[] args) {
         if (!cmd.getName().equalsIgnoreCase(getName())) {
             return false;
         }
-
-        if (!(sender instanceof Player p)) {
-            sender.sendMessage("§c此指令必须由玩家执行！");
-            return true;
-        }
         if (!sender.hasPermission(getPermissionString())) {
             sender.sendMessage("§c你没有权限执行这个指令！");
             return true;
         }
-        if (args.length != 1) {
-            sender.sendMessage("§c指令参数错误！使用方法为/join <游戏名称>");
+        if (args.length != 1 && args.length != 2) {
+            sender.sendMessage("§c指令参数错误！使用方法为/join <游戏名称> [玩家]");
             return true;
+        }
+        Player target;
+        if (args.length == 1) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("§c此指令必须由玩家执行！");
+                return true;
+            }
+            target = (Player) sender;
+        } else {
+            if (!sender.hasPermission(getOthersPermissionString())) {
+                sender.sendMessage("§c你没有权限执行这个指令！");
+                return true;
+            }
+            target = Bukkit.getPlayer(args[1]);
+            if (target == null) {
+                sender.sendMessage("§c未找到玩家 " + args[1] + "！");
+                return true;
+            }
         }
         String gameName = args[0];
         List<Game> matchingGames = new ArrayList<>();
@@ -53,14 +71,14 @@ public class Join extends GameUtilsCommand implements TabCompleter {
             sender.sendMessage("§c找到多个名称为" + gameName + "的游戏，加入已取消！可能是游戏字母相同但大小写不同！");
             return true;
         }
-        Game currentGame = GameUtils.inst().getGame(p);
+        Game currentGame = GameUtils.inst().getGame(target);
         Game newGame = matchingGames.getFirst();
         if (currentGame == newGame) {
-            sender.sendMessage("§c你已经在这个游戏中了！");
+            sender.sendMessage("§c你/目标玩家已经在这个游戏中了！");
             return true;
         }
-        currentGame.removePlayer(p);
-        GameUtils.inst().join(p, newGame);
+        currentGame.removePlayer(target);
+        GameUtils.inst().join(target, newGame);
         return true;
     }
 
@@ -69,10 +87,14 @@ public class Join extends GameUtilsCommand implements TabCompleter {
         if (!command.getName().equalsIgnoreCase(getName())) {
             return new ArrayList<>();
         }
-        if (args.length != 1) {
+        if (args.length == 1) {
+            List<String> gameNames = GameUtils.inst().getGames().stream().map(Game::getName).toList();
+            return getMatchingCompletions(args[0], gameNames);
+        } else if (args.length == 2) {
+            List<String> playerNames = Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
+            return getMatchingCompletions(args[1], playerNames);
+        } else {
             return new ArrayList<>();
         }
-        List<String> gameNames = GameUtils.inst().getGames().stream().map(Game::getName).toList();
-        return getMatchingCompletions(args[0], gameNames);
     }
 }
